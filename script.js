89be2cc8853e85e31de01716bb584bbb3ae76aae
B2M Development <development@b2m-solutions.com>
2014-06-20 10:40:32 +0100

    http -> https

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ $(function() {
     var startedAt = new Date().getTime();
 
     $.ajax({
-      url: 'http://elemez.com/raw/1/latest',
+      url: 'https://elemez.com/raw/1/latest',
       beforeSend: function(xhr) {
         xhr.setRequestHeader('token', token);
       },
@@ -88,7 +88,7 @@ $(function() {
     function poll() {
       var lastKeyQueryParameter = lastKey ? '&lastkey=' + lastKey : '';
       $.ajax({
-        url: 'http://elemez.com/raw/1?sort=asc' + lastKeyQueryParameter,
+        url: 'https://elemez.com/raw/1?sort=asc' + lastKeyQueryParameter,
         beforeSend: function(xhr) {
           xhr.setRequestHeader('token', token);
         },
